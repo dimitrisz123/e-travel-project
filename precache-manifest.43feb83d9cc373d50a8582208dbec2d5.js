1e933bcf7517287298a5242d9701c6f4f53b1e0b
Dimitris Zer <dimitris-007@hotmail.com>
2019-07-25 19:39:22 +0300

    Updates

---
+++ precache-manifest.43feb83d9cc373d50a8582208dbec2d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c60ba82143a524d7740e9a1fdcae38",
+    "url": "/e-travel-project/index.html"
+  },
+  {
+    "revision": "0dd9a55c1b85d80d09cb",
+    "url": "/e-travel-project/static/css/main.3aa3fb1d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/e-travel-project/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0dd9a55c1b85d80d09cb",
+    "url": "/e-travel-project/static/js/main.7a7bd434.chunk.js"
+  },
+  {
+    "revision": "15e51644be244bfbe7a6",
+    "url": "/e-travel-project/static/js/runtime~main.38376e4e.js"
+  }
+]);
